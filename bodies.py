a8850adee3214c073f78fa3f440dc011ba9881a8
Nurmela Mika M <nurmika@cubbli18-gold.local>
2020-01-18 12:51:29 +0200

    first commit

---
+++ bodies.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tkinter import *
+master = Tk()
+	
+
+def canvas():
+	canvas_width = 800
+	canvas_height = 400
+	w = Canvas(master,width=canvas_width,height=canvas_height)
+	w.pack()
+	mainloop()
+
+def main():
+	canvas()
+			
+if __name__ == "__main__":
+    main()
